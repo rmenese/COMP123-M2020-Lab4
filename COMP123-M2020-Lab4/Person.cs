0fbb5c30821cf4ecf0f9a885a6ca4033547f80a3
Remedios Meneses <remedios8meneses@gmail.com>
2020-06-20 00:19:31 -0400

    instantiated object of type Person

---
+++ COMP123-M2020-Lab4/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_M2020_Lab4
+{
+    class Person
+    {
+        //PRIVATE INSTANCE VARIABLES
+
+
+        //PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //CONSTRUCTOR(S)
+
+        public Person(string name, int age)
+        {
+            //property initialization
+            Name = name;
+            Age = age;
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+        public void SaysHello()
+        {
+            Console.WriteLine($"{Name} says Hello.");
+        }
+    }
+}
--- COMP123-M2020-Lab4/Program.cs
+++ COMP123-M2020-Lab4/Program.cs
@@ -10,7 +10,13 @@ namespace COMP123_M2020_Lab4
     {
         static void Main(string[] args)
         {
+            // instantiate object of type Person
+            Person rem = new Person(name:"Rem", age: 40); //fyi I just turned 40 today, smile!
 
+            // use saysHello method with . accessor
+            rem.SaysHello();
+
+            Console.ReadLine();
         }
     }
 }
